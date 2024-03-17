f63ba5d6372b132a291e17c1c68c69a120e0290c
Hashane <jhashane@gmail.com>
2024-03-17 22:10:01 +0530

    Redesigned Dashboard

--- lib/screens/Home/Dashboard.dart
+++ lib/screens/Home/Dashboard.dart
@@ -36,8 +36,6 @@ class _StatsPageState extends State<Dashboard> {
             IconButton(
               icon: Icon(Icons.logout),
               onPressed: () {
-                // Add logout functionality here
-                // For example, you can navigate to the login page
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => LoginScreen()),
@@ -67,77 +65,43 @@ class _StatsPageState extends State<Dashboard> {
         "color": Colors.red,
         "label": "Expense",
         "cost": "\$2645.50"
+      },
+      {
+        "icon": Icons.arrow_forward,
+        "color": Colors.red,
+        "label": "Expense",
+        "cost": "\$2645.50"
       }
     ];
     return SingleChildScrollView(
       child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(color: Colors.white, boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.01),
-                spreadRadius: 10,
-                blurRadius: 3,
-                // changes position of shadow
-              ),
-            ]),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  top: 20, right: 20, left: 20, bottom: 25),
-              child: Column(
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 30, right: 20, left: 20, bottom: 25),
+          child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // Row(
-                  //     crossAxisAlignment: CrossAxisAlignment.start,
-                  //     children: List.generate(months.length, (index) {
-                  //       return GestureDetector(
-                  //         onTap: () {
-                  //           setState(() {
-                  //             activeDay = index;
-                  //           });
-                  //         },
-                  //         child: Container(
-                  //           width: (MediaQuery.of(context).size.width - 40) / 6,
-                  //           child: Column(
-                  //             children: [
-                  //               Text(
-                  //                 months[index]['label'],
-                  //                 style: TextStyle(fontSize: 10),
-                  //               ),
-                  //               SizedBox(
-                  //                 height: 10,
-                  //               ),
-                  //               Container(
-                  //                 decoration: BoxDecoration(
-                  //                     color: activeDay == index
-                  //                         ? Colors.pink
-                  //                         : Colors.black.withOpacity(0.02),
-                  //                     borderRadius: BorderRadius.circular(5),
-                  //                     border: Border.all(
-                  //                         color: activeDay == index
-                  //                             ? Colors.pink
-                  //                             : Colors.black.withOpacity(0.1))),
-                  //                 child: Padding(
-                  //                   padding: const EdgeInsets.only(
-                  //                       left: 12, right: 12, top: 7, bottom: 7),
-                  //                   child: Text(
-                  //                     months[index]['day'],
-                  //                     style: TextStyle(
-                  //                         fontSize: 10,
-                  //                         fontWeight: FontWeight.w600,
-                  //                         color: activeDay == index
-                  //                             ? Colors.white
-                  //                             : Colors.black),
-                  //                   ),
-                  //                 ),
-                  //               )
-                  //             ],
-                  //           ),
-                  //         ),
-                  //       );
-                  //     }))
+                  Text(
+                    'Hello, User!',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/profile_image.jpg'), // Your profile image asset
+                  ),
                 ],
               ),
-            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Wrap(
+            spacing: 20,
+            children: [
+              _buildInfoBox("Income", "\$6593.75", CustomTheme.primaryColor),
+              _buildInfoBox("Expense", "\$2645.50", Colors.red),
+             ],
           ),
           SizedBox(
             height: 20,
@@ -201,92 +165,71 @@ class _StatsPageState extends State<Dashboard> {
               ),
             ),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Wrap(
-              spacing: 20,
-              children: List.generate(expenses.length, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    // if (expenses[index]["label"] == "Income") {
-                    //   // Navigate to the IncomeDetailsPage
-                    //   Navigator.of(context).push(
-                    //     MaterialPageRoute(
-                    //       builder: (context) => RevenuesScreen(),
-                    //     ),
-                    //   );
-                    // } else {
-                    //   Navigator.of(context).push(
-                    //     MaterialPageRoute(
-                    //       builder: (context) => ExpensesScreen(),
-                    //     ),
-                    //   );
-                    // }
-                  },
-                  child: Container(
-                    width: (size.width - 60) / 2,
-                    height: 170,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.01),
-                            spreadRadius: 10,
-                            blurRadius: 3,
-                            // changes position of shadow
-                          ),
-                        ]),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 25, right: 25, top: 20, bottom: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: 40,
-                            height: 40,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: expenses[index]['color']),
-                            child: Center(
-                                child: Icon(
-                              expenses[index]['icon'],
-                              color: Colors.white,
-                            )),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                expenses[index]['label'],
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 13,
-                                    color: Color(0xff67727d)),
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                expenses[index]['cost'],
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              }))
         ],
       ),
     );
   }
+
+  Widget _buildInfoBox(String label, String cost, Color color) {
+    IconData icon = Icons.arrow_back;
+    Color iconColor = Colors.red;
+
+    if (label == "Income") {
+      icon = Icons.arrow_back;
+      iconColor = Colors.green;
+    } else if (label == "Expense") {
+      icon = Icons.arrow_forward;
+      iconColor = Colors.red;
+    }
+
+    var size = MediaQuery.of(context).size;
+
+    return Container(
+      width: (size.width - 60) / 2,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.01),
+            spreadRadius: 10,
+            blurRadius: 3,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              icon,
+              color: iconColor,
+              size: 40,
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 13,
+                color: Color(0xff67727d),
+              ),
+            ),
+            SizedBox(height: 4),
+            FittedBox(
+              child: Text(
+                cost,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
